165375d0ed2558cdc311f35653cec3c65db1b8c0
kirillkrohmal <krohmal_kirill@mail.ru>
2020-05-22 14:19:11 +0300

    counters1

---
+++ src/main/java/com/example/sberbank/counters/CountersApplication.java
@@ -0,0 +1,13 @@
+package com.example.sberbank.counters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountersApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/sberbank/counters/controller/CounterController.java
@@ -0,0 +1,52 @@
+package com.example.sberbank.counters.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Set;
+
+@RestController
+@RequestMapping("api/v1/counter")
+public class CounterController {
+    private HashMap <String, Integer> counters = new HashMap<String, Integer>();
+
+    @GetMapping("keys")
+    public Set<String> getKeys() {
+        Set<String> keySet = counters.keySet();
+
+        return keySet;
+    }
+
+    @GetMapping("get/{name}")
+    public int getCounter(@PathVariable("name") String name){
+        Integer value = counters.get(name);
+        if (value == null) {
+            value = new Integer(0);
+
+            counters.put(name, value);
+        }
+
+        int n = value.intValue();
+        n++;
+
+        counters.put(name, new Integer(n));
+
+        return n;
+    }
+
+    @DeleteMapping("delete/{name}")
+    public int delete(@PathVariable("name") String name) {
+
+        Integer value = counters.get(name);
+        if (value == null) {
+            value = new Integer(0);
+
+            counters.remove(name, value);
+        }
+
+        int n = value.intValue();
+        n--;
+
+        return n;
+    }
+}
